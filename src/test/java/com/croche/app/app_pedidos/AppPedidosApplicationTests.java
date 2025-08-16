11b398a2f6f23a2dd73df7f943c8f8d781c2b9c7
Nycolas Rodrigues <rodriguesnycolas22@outlook.com>
2025-08-15 22:17:34 -0300

    app-croche-pedidos

---
+++ src/test/java/com/croche/app/app_pedidos/AppPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croche.app.app_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
